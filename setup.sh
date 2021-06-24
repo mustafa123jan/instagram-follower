5b8e1838f6b89bd4712126db9d56ffc0151650f5
Mustafa Kamgar (MK) <67920337+mustafa123jan@users.noreply.github.com>
2021-06-24 11:42:53 +0430

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+clear
+apt update
+apt upgrade
+apt install figlet -y
+figlet Basic Installation
+apt install toilet -y
+apt install cowsay -y
+apt install nano -y
+apt install ruby -y
+gem install lolcat
+apt install git curl openssh openssl openssl-tool -y
+figlet -f big FINISHED !!! | lolcat
+echo ""
+echo ""
+echo " JOIN TO MY CHANNEL 😉 " | lolcat
+xdg-open https://t.me/the_world_of_hacking
+echo ""
+sleep 10 
+echo ""
+echo " code recorded by mustafa kamgar "
+sleep 5
+sleep 10
+echo " create account on cashkaro.com "
+echo " set your browser "
+termux-open-url https://cashkaro.com?r=8026255&fname=Rixon
+echo ""
